51e469453a355dad493314c628cf858fe2d38071
Gian Paolo Calica <25701594+gcalica@users.noreply.github.com>
2018-11-03 09:52:31 -1000

    tiny commit. updated dependencies, settings, and wrapper

--- app/imports/api/Wrapper/Wrapper.js
+++ app/imports/api/Wrapper/Wrapper.js
@@ -1,12 +1,10 @@
-/* Back End Notes: Document all functions. To create a JSDoc, place a cursor on top of the function.
+/* Document all functions. To create a JSDoc, place a cursor on top of the function.
   Type /** and then press Enter.
 */
+import * as _ from 'meteor/underscore';
 
-/**
- *
- * @param test
- * @returns {*}
- */
-export function test(test) {
-  return test;
+export function getTransitionData(event_ids, location_ids, building_ids, category_ids) {
+  _.each(event_ids)
 }
+
+console.log(getTransitionData());
