ea21d7658bbcb3b68e4e2f886e64f28bd3a49196
Hyosun Song <hyosuns@hawaii.edu>
2018-11-02 22:53:00 -1000

    inputform1

---
+++ app/imports/ui/components/BagAdmin.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { Card, Image } from 'semantic-ui-react';
+import PropTypes from 'prop-types';
+import { withRouter } from 'react-router-dom';
+
+/** Renders a single row in the List Stuff table. See pages/ListStuff.jsx. */
+class BagAdmin extends React.Component {
+  render() {
+    return (
+        <Card centered>
+          <Card.Content>
+            <Card.Header>
+              {this.props.bag.id}
+            </Card.Header>
+            <Card.Meta>
+              {this.props.bag.type}
+            </Card.Meta>
+            <Card.Description>
+              {this.props.bag.weight}
+            </Card.Description>
+            <Card.Description>
+              {this.props.bag.volume}
+            </Card.Description>
+          </Card.Content>
+          <Card.Content extra>
+            {this.props.bag.owner}
+          </Card.Content>
+        </Card>
+    );
+  }
+}
+
+/** Require a document to be passed to this component. */
+BagAdmin.propTypes = {
+  bag: PropTypes.object.isRequired,
+};
+
+/** Wrap this component in withRouter since we use the <Link> React Router element. */
+export default withRouter(BagAdmin);
