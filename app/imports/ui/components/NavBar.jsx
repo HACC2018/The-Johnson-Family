9740e6e0ec5d707ddfd5f16eebddb9e99bed0f85
Pauline-Peihan-Wu <Pauline-Peihan-Wu@users.noreply.github.com>
2018-11-04 22:57:21 -1000

    Update NavBar

--- app/imports/ui/components/NavBar.jsx
+++ app/imports/ui/components/NavBar.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import { Meteor } from 'meteor/meteor';
 import { withTracker } from 'meteor/react-meteor-data';
 import { withRouter, NavLink } from 'react-router-dom';
-import { Menu, Dropdown, Image } from 'semantic-ui-react';
+import { Menu, Dropdown } from 'semantic-ui-react';
 import { Roles } from 'meteor/alanning:roles';
 
 /** The NavBar appears at the top of every page. Rendered by the App Layout component. */
@@ -26,52 +26,50 @@ class NavBar extends React.Component {
       marginRight: '20px',
       backgroundColor: 'black',
     };
-    const noImgPadding = {
-      padding: '0px',
-    };
-    return (
-        <Menu style={menuStyle} attached="top" borderless inverted>
-          {/* LOGO ITEM */}
 
-          {/* Additional menu when signed in as admin */}
-          {Roles.userIsInRole(Meteor.userId(), 'admin') ? (
-              <Menu.Item as={NavLink} activeClassName="active" exact to="/admin" key='admin'>Input Audit</Menu.Item>,
-              <Menu.Item as={NavLink} activeClassName="active" exact to="/addlocations" key='add'>Add Locations</Menu.Item>,
-              <Menu.Item as={NavLink} activeClassName="active" exact to="/listlocations" key='list'>List Locations</Menu.Item>
-            ) : ''}
-          {/* Top right not signed in */}
-          <Menu.Item style={itemStyle} position="right">
-            {this.props.currentUser === '' ? (
-                <Dropdown text="Login" pointing="top right" icon={'user'}>
+     return <Menu style={menuStyle} attached="top" borderless inverted>
+      {/* LOGO ITEM */}
+
+      {/* Additional menu when signed in as admin */}
+      {Roles.userIsInRole(Meteor.userId(), 'admin') ? (
+          <Menu.Item as={NavLink} activeClassName="active" exact to="/admin" key='admin'>Input Audit</Menu.Item>,
+              <Menu.Item
+                  as={NavLink} activeClassName="active" exact to="/addlocations" key='add'>Add Locations</Menu.Item>,
+              <Menu.Item
+                  as={NavLink} activeClassName="active" exact to="/listlocations" key='list'>List Locations</Menu.Item>
+      ) : ''}
+      {/* Top right not signed in */}
+      <Menu.Item style={itemStyle} position="right">
+        {this.props.currentUser === '' ? (
+            <Dropdown text="Login" pointing="top right" icon={'user'}>
+              <Dropdown.Menu>
+                <Dropdown.Item icon="user" text="Sign In" as={NavLink} exact to="/signin"/>
+                <Dropdown.Item icon="add user" text="Sign Up" as={NavLink} exact to="/signup"/>
+              </Dropdown.Menu>
+            </Dropdown>
+        ) : (
+            <Menu.Item style={menuStyle}>
+              <Menu.Item position='right' as={NavLink} activeClassName="active" style={itemStyle}
+                         exact to="/Home" key='Home'>Home
+              </Menu.Item>
+              <Menu.Item as={NavLink} activeClassName="active" style={itemStyle}
+                         exact to="/list" key='list'>Submit Data
+              </Menu.Item>
+              <Menu.Item as={NavLink} activeClassName="active" style={itemStyle}
+                         exact to="/dash" key='dash'>Dashboard
+              </Menu.Item>
+              <Menu.Item style={leftItemStyle}>
+                <Dropdown pointing="top right" icon={'user'}>
                   <Dropdown.Menu>
-                    <Dropdown.Item icon="user" text="Sign In" as={NavLink} exact to="/signin"/>
-                    <Dropdown.Item icon="add user" text="Sign Up" as={NavLink} exact to="/signup"/>
+                    <Dropdown.Item icon="sign out" text="Sign Out" as={NavLink} exact to="/signout"/>
                   </Dropdown.Menu>
                 </Dropdown>
-            ) : (
-                <Menu.Item style={menuStyle}>
-                  <Menu.Item position='right' as={NavLink} activeClassName="active" style={itemStyle}
-                             exact to="/Home" key='Home'>Home
-                  </Menu.Item>
-                  <Menu.Item as={NavLink} activeClassName="active" style={itemStyle}
-                             exact to="/list" key='list'>Submit Data
-                  </Menu.Item>
-                  <Menu.Item as={NavLink} activeClassName="active" style={itemStyle}
-                             exact to="/dash" key='dash'>Dashboard
-                  </Menu.Item>
-                  <Menu.Item style={leftItemStyle}>
-                    <Dropdown pointing="top right" icon={'user'}>
-                      <Dropdown.Menu>
-                        <Dropdown.Item icon="sign out" text="Sign Out" as={NavLink} exact to="/signout"/>
-                      </Dropdown.Menu>
-                    </Dropdown>
-                  </Menu.Item>
-                </Menu.Item>
-            )}
-            {/* Top right when user is signed in ABOVE */}
-          </Menu.Item>
-        </Menu>
-    );
+              </Menu.Item>
+            </Menu.Item>
+        )}
+        {/* Top right when user is signed in ABOVE */}
+      </Menu.Item>
+    </Menu>;
   }
 }
 
