88c1a115340d420b8c988c18a0673fb0f20f2107
Pauline-Peihan-Wu <Pauline-Peihan-Wu@users.noreply.github.com>
2018-11-04 15:32:34 -1000

    Landing done

--- app/imports/ui/components/ThirdBackground.jsx
+++ app/imports/ui/components/ThirdBackground.jsx
@@ -8,17 +8,17 @@ export default class ThirdBackground extends React.Component {
         <div className="third-background">
           <Grid container textAlign="center" verticalAlign="middle" style={gridStyle} row={3}>
             <Grid.Row>
-              <Header as="h2" inverted>
+              <Header as="h2">
                 Fighting to reduce greenhouse gas emissions at all of UH campuses.
               </Header>
             </Grid.Row>
             <Grid.Row>
-              <Header as="h2" inverted>
+              <Header as="h2">
                 Information about the waste audit
               </Header>
             </Grid.Row>
             <Grid.Row>
-              <Header as="h2" inverted>
+              <Header as="h2">
                 Pictures from 2017 Waste Audit
               </Header>
             </Grid.Row>
