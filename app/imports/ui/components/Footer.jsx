5063520db92f4fa7ea19e8f1ecb348c0e356c213
Pauline-Peihan-Wu <Pauline-Peihan-Wu@users.noreply.github.com>
2018-11-05 18:15:41 -1000

    Footer update

--- app/imports/ui/components/Footer.jsx
+++ app/imports/ui/components/Footer.jsx
@@ -1,16 +1,23 @@
 import React from 'react';
+import { Image } from 'semantic-ui-react';
 
 /** The Footer appears at the bottom of every page. Rendered by the App Layout component. */
 class Footer extends React.Component {
   render() {
+    const imgStyle = {
+      width: '150px',
+      height: '50px',
+      position: 'center',
+    };
+
     const divStyle = { paddingTop: '15px' };
     return (
         <footer>
-          <div style={divStyle} className="ui center aligned container">
-            <hr />
-              Department of Information and Computer Sciences <br />
-              University of Hawaii<br />
-              Honolulu, HI 96822
+          <div className='footer-background'>
+          <div style={divStyle} className="ui center aligned container"><Image
+                src="https://cdn.discordapp.com/attachments/508521520109453314/508834015080349706/gatherwhite.png"
+                style={imgStyle} centered/>
+          </div>
           </div>
         </footer>
     );
