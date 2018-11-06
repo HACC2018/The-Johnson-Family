293af208d0dd77b4aa85680ce13220fa414fdbbf
Gian Paolo Calica <25701594+gcalica@users.noreply.github.com>
2018-11-06 10:41:54 -1000

    Added working getCollectionNames method in Wrapper

--- app/imports/ui/pages/giantestpage.jsx
+++ app/imports/ui/pages/giantestpage.jsx
@@ -5,12 +5,16 @@ import { Locations } from '/imports/api/Locations/Locations';
 // import Location from '/imports/ui/components/Location';
 import { withTracker } from 'meteor/react-meteor-data';
 import PropTypes from 'prop-types';
+import { getLocationNames } from '../../api/Wrapper/Wrapper.js';
+
 
 /** Renders a table containing all of the Stuff documents. Use <StuffItem> to render each row. */
 class giantestpage extends React.Component {
+
   constructor(props) {
     super(props);
     this.onClick = this.onClick.bind(this);
+    this.onClick2 = this.onClick2.bind(this);
     this.deleteClick = this.deleteClick.bind(this);
   }
   /** Notify the user of the results of the submit. If successful, clear the form. */
@@ -34,29 +38,38 @@ class giantestpage extends React.Component {
     Locations.insert({ name: "TestName", street: "TestStreet", city: "TestCity", state: "TestState", zip_code: "96797" }, this.insertCallback);
   }
 
+  onClick2() {
+    Locations.insert({ name: "TestName2", street: "TestStreet2", city: "TestCity2", state: "TestState2", zip_code: "96797" }, this.insertCallback);
+  }
+
   deleteClick() {
     const id = this.props.location[0]._id;
     Locations.remove(id, this.deleteCallback);
   }
+
   /** If the subscription(s) have been received, render the page, otherwise show a loading icon. */
   render() {
-
+    const getLocations = getLocationNames(1);
+    console.log({getLocations});
     // console.log(aLoc[name]);
     // TODO - implement: console.log(db.getLocationsCollection()[0]);
     console.log('hi me gian');
-    console.log(this.props.location[0]);
-    console.log(this.props.location[1]);
-    console.log(this.props.location);
+    // console.log(this.props.location[0]);
+    // console.log(this.props.location[1]);
+    // console.log(this.props.location);
     return (this.props.ready) ? this.renderPage() : <Loader>Getting data</Loader>;
   }
 
   /** Render the page once subscriptions have been received. */
   renderPage() {
+    const getLocations = getLocationNames();
     return (
         <Container>
               {/*{this.props.location.map((location) => <Location key={location._id} location={location} />)}*/}
           {/*{this.props.location.name}*/}
+          {/*<div>{getLocations}</div>*/}
           <Button basic onClick={this.onClick}>Add TestLocation</Button>
+          <Button basic onClick={this.onClick2}>Add TestLocation</Button>
           <Button basic onClick={this.deleteClick}>Delete TestLocation</Button>
         </Container>
     );
@@ -67,7 +80,6 @@ class giantestpage extends React.Component {
 giantestpage.propTypes = {
   location: PropTypes.array.isRequired,
   ready: PropTypes.bool.isRequired,
-
 };
 
 /** withTracker connects Meteor data to React components. https://guide.meteor.com/react.html#using-withTracker */
