3f12744f8ecc951e70d142ef27a3afcc2064fded
Pauline-Peihan-Wu <Pauline-Peihan-Wu@users.noreply.github.com>
2018-11-05 20:41:58 -1000

    Gian Test Page

---
+++ app/imports/ui/pages/giantestpage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Test from '../components/Test';
+
+export default class GianTest extends React.Component {
+
+  render() {
+    return (
+        <div>
+          <Test/>
+        </div>
+    );
+  }
+}
