d00c217e315f59e149140d65a63fc3b8945c6621
Gian Paolo Calica <25701594+gcalica@users.noreply.github.com>
2018-11-05 20:59:04 -1000

    commit

--- app/imports/ui/pages/giantestpage.jsx
+++ app/imports/ui/pages/giantestpage.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
-import Test from '../components/Test';
+// import Test from '../components/Test';
+import Locations from '/imports/api/Locations/Locations';
 
+const locations = Locations.find({}).fetch();
+console.log(locations);
 export default class GianTest extends React.Component {
-
   render() {
+
     return (
         <div>
-          <Test/>
+          {locations}
         </div>
     );
   }
