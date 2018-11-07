05d0f98769581e86a5c8cb819b26836c2c6d78df
Gian Paolo Calica <25701594+gcalica@users.noreply.github.com>
2018-11-06 16:39:19 -1000

    Copy pasted Sam's work into my branch. Added functions for getting the individual field values for Locations collection

--- app/imports/ui/pages/giantestpage.jsx
+++ app/imports/ui/pages/giantestpage.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
-import { Meteor } from 'meteor/meteor';
+// import { Meteor } from 'meteor/meteor';
 import { Button, Container, Loader } from 'semantic-ui-react';
 import { Locations } from '/imports/api/Locations/Locations';
-// import Location from '/imports/ui/components/Location';
 import { withTracker } from 'meteor/react-meteor-data';
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 import * as db from '../../api/Wrapper/Wrapper.jsx';
 
 
@@ -14,16 +13,7 @@ class giantestpage extends React.Component {
   constructor(props) {
     super(props);
     this.onClick = this.onClick.bind(this);
-    this.onClick2 = this.onClick2.bind(this);
     this.deleteClick = this.deleteClick.bind(this);
-  }
-  /** Notify the user of the results of the submit. If successful, clear the form. */
-  insertCallback(error) {
-    if (error) {
-      Bert.alert({ type: 'danger', message: `Insert failed: ${error.message}` });
-    } else {
-      Bert.alert({ type: 'success', message: 'Insert succeeded' });
-    }
   }
 
   deleteCallback(error) {
@@ -43,10 +33,6 @@ class giantestpage extends React.Component {
     }
   }
 
-  onClick2() {
-    Locations.insert({ name: "TestName2", street: "TestStreet2", city: "TestCity2", state: "TestState2", zip_code: "96797" }, this.insertCallback);
-  }
-
   deleteClick() {
     const id = this.props.location[0]._id;
     Locations.remove(id, this.deleteCallback);
@@ -54,13 +40,8 @@ class giantestpage extends React.Component {
 
   /** If the subscription(s) have been received, render the page, otherwise show a loading icon. */
   render() {
-    console.log("divider");
-    const getLocation = db.getLocationNames();
-    console.log(getLocation);
-    // console.log(aLoc[name]);
-    // console.log(this.props.location);
-    // console.log(this.props.location[1]);
-    // console.log(this.props.location);
+    const test = db.getCollectionValues(Locations);
+    console.log(test);
     return (this.props.ready) ? this.renderPage() : <Loader>Getting data</Loader>;
   }
 
@@ -68,22 +49,12 @@ class giantestpage extends React.Component {
   renderPage() {
     return (
         <Container>
-          {/*{this.props.location.map((location) => <Location key={location._id} location={location} />)}*/}
-          {/*{this.props.location.name}*/}
-          {/*<div>{getLocations}</div>*/}
           <Button basic onClick={this.onClick}>Add TestLocation</Button>
-          <Button basic onClick={this.onClick2}>Add TestLocation</Button>
           <Button basic onClick={this.deleteClick}>Delete TestLocation</Button>
         </Container>
     );
   }
 }
-
-/** Require an array of Stuff documents in the props. */
-giantestpage.propTypes = {
-  location: PropTypes.array.isRequired,
-  ready: PropTypes.bool.isRequired,
-};
 
 /** withTracker connects Meteor data to React components. https://guide.meteor.com/react.html#using-withTracker */
 export default withTracker(() => {
