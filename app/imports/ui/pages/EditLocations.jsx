a4bea6ee25cdbc30a053ef4cd652f83bb51e3dc9
Hyosun Song <hyosuns@hawaii.edu>
2018-11-07 18:40:25 -1000

    linking submitdata and editlocations pages

---
+++ app/imports/ui/pages/EditLocations.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Grid, Header } from 'semantic-ui-react';
+import AdminOptions from '../components/AdminOptions';
+
+/** A simple static component to render some text for the landing page. */
+class EditLocations extends React.Component {
+  render() {
+
+    const style = {
+      margin: '0px',
+      paddingTop: '30px',
+      paddingBottom: '500px',
+    };
+
+    const bodyStyle = {
+      paddingTop: '30px',
+      paddingBottom: '30px',
+    };
+
+    return (
+        <Grid container style={bodyStyle} divided='vertically'>
+          <AdminOptions/>
+
+          <Grid.Row>
+            <Header as="h1" style={style}>
+              EDIT LOCATIONS PAGE
+            </Header>
+          </Grid.Row>
+        </Grid>
+    );
+  }
+}
+
+export default EditLocations;
