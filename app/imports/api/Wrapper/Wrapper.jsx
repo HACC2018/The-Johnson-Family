e9a1ea07e3fa2ef8ff6a99283c3871329a7268a4
SamZyskowski <zyskowskisam@gmail.com>
2018-11-07 21:48:08 -1000

    altered and tested all three methods, all running

--- app/imports/api/Wrapper/Wrapper.jsx
+++ app/imports/api/Wrapper/Wrapper.jsx
@@ -37,21 +37,28 @@ function getCollection(collectionKey) {
  * Returns an array that holds within it the amount of campuses that match the three tested names.
  * @return {Array} An array of how many campuses of each exist.
  */
-export function getCompositionOfLocations(){
+export function getCompositionOfLocations() {
   const cursor = getCollection(1);
   let result = [];
   let final = [];
   cursor.forEach((doc) => result.push(doc.name));
 
-  const KCC  = _.filter(result, function(entry) {return entry.name.toLowerCase().contains('kcc')});
+  const KCC = _.filter(result, function (entry) {
+    return entry.toLowerCase().includes('kcc')
+  });
+
   const KCCAmount = KCC.length;
   final.push(KCCAmount);
 
-  const UH  = _.filter(result, function(entry) {return entry.name.toLowerCase().contains('uh')});
+  const UH = _.filter(result, function (entry) {
+    return entry.toLowerCase().includes('uh')
+  });
   const UHAmount = UH.length;
   final.push(UHAmount);
 
-  const WestOahu = _.filter(result, function(entry) {return entry.name.toLowerCase().contains('westoahu')});
+  const WestOahu = _.filter(result, function (entry) {
+    return entry.toLowerCase().includes('westoahu')
+  });
   const westOahuAmount = WestOahu.length;
   final.push(westOahuAmount);
 
@@ -62,35 +69,89 @@ export function getCompositionOfLocations(){
  * Returns an array similar to getCompositionOfLocations, except it filters through all the trash bags and returns the amount of each per category type.
  * @return {Array} An array containing a list of numbers referencing the amount of trash bags listed under certain trash types.
  */
-export function getCategoryValues(){
-  const cursor = getCollection(4);
+export function getCategoryValues() {
+  const cursor = getCollection(5);
   let all = [];
   let final = [];
-  cursor.forEach((doc) => all.push(doc.category_id));
+  cursor.forEach((doc) => all.push(doc.parent_id));
 
-  const paper  = _.filter(all, function(entry) {return entry.category_id.toLowerCase().contains('paper')});
+  const paper = _.filter(all, function (entry) {
+    return entry.toLowerCase().includes('paper')
+  });
   const paperAmount = paper.length;
   final.push(paperAmount);
 
-  const plastic  = _.filter(all, function(entry) {return entry.category_id.toLowerCase().contains('plastic')});
+  const plastic = _.filter(all, function (entry) {
+    return entry.toLowerCase().includes('plastic')
+  });
   const plasticAmount = plastic.length;
   final.push(plasticAmount);
 
-  const glass  = _.filter(all, function(entry) {return entry.category_id.toLowerCase().contains('glass')});
+  const glass = _.filter(all, function (entry) {
+    return entry.toLowerCase().includes('glass')
+  });
   const glassAmount = glass.length;
   final.push(glassAmount);
 
-  const metal  = _.filter(all, function(entry) {return entry.category_id.toLowerCase().contains('metal')});
+  const metal = _.filter(all, function (entry) {
+    return entry.toLowerCase().includes('metal')
+  });
   const metalAmount = metal.length;
   final.push(metalAmount);
 
-  const organics  = _.filter(all, function(entry) {return entry.category_id.toLowerCase().contains('organics')});
+  const organics = _.filter(all, function (entry) {
+    return entry.toLowerCase().includes('organics')
+  });
   const organicsAmount = organics.length;
   final.push(organicsAmount);
 
   return final;
+}
+
+/**
+ * Returns an array of blah blah blah
+ */
+export function getTrashBagsByDate() {
+  let final = [];
+  const trash = getCollection(4);
+  let allTrash = [];
+  trash.forEach((doc) => allTrash.push(doc.event_id));
+
+  const event = getCollection(3);
+  let allEvents = [];
+  event.forEach((doc) => allEvents.push(doc.date));
+
+  let springEvents = _.filter(allEvents, function (spring) {
+    return (spring.toString().includes('January')) || (spring.toString().includes('February')) || (spring.toString().includes('March')) || (spring.toString().includes('April')) || (spring.toString().includes('May'))
+  });
+
+  let spring17 = _.filter(springEvents, function (spring) {
+    return spring.toString().includes('2017')
+  });
+  let spring18 = _.filter(springEvents, function (spring) {
+    return spring.toString().includes('2018')
+  });
+
+  let fallEvents = _.filter(allEvents, function (fall) {
+    return (fall.toString().includes('August')) || (fall.toString().includes('September')) || (fall.toString().includes('October')) || (fall.toString().includes('November')) || (fall.toString().includes('December'))
+  });
+
+  let fall17 = _.filter(fallEvents, function (fall) {
+    return fall.toString().includes('2017')
+  });
+  let fall18 = _.filter(fallEvents, function (fall) {
+    return fall.toString().includes('2018')
+  });
+
+  final.push(spring17.length);
+  final.push(fall17.length);
+  final.push(spring18.length);
+  final.push(fall18.length);
+
+  return final;
 
 }
+
 /**
  * Returns the values of the specified collection.
  * @param collectionKey the key indicating which collection to retrieve data from. See #information channel on Discord.
@@ -137,12 +198,12 @@ export function addNewLocation(name, street, city, state, zip_code) {
   let uniqueZipCodes = [];
   cursor.forEach((doc) => uniqueZipCodes.push(doc.zip_code));
   uniqueZipCodes = _.uniq(uniqueZipCodes);
-  if (uniqueStreets.includes(street) && uniqueCities.includes(city) && uniqueStates.includes(state) && uniqueZipCodes.includes(zip_code)){
+  if (uniqueStreets.includes(street) && uniqueCities.includes(city) && uniqueStates.includes(state) && uniqueZipCodes.includes(zip_code)) {
     return false;
   } else {
-          Locations.insert({ name: name, street: street, city: city, state: state, zip_code: zip_code });
-          return true;
-        }
+    Locations.insert({ name: name, street: street, city: city, state: state, zip_code: zip_code });
+    return true;
+  }
 }
 
 /**
@@ -168,38 +229,22 @@ export function addNewCategory(name, parent_id, level) {
 
   if (uniqueNames.includes(name) && uniqueParent_id.includes(parent_id) && uniqueLevel.includes(level)) {
     return false;
-      }
-      else {
-        Categories.insert({ name: name, parent_id: parent_id, level: level })
-        return true;
+  }
+  else {
+    Categories.insert({ name: name, parent_id: parent_id, level: level });
+    return true;
 
-      }
+  }
 }
 
 /**
  * Returns true if the document was added to the Forms collection successfully. False, otherwise.
  * This function guarantees that the Admin cannot add duplicate Forms. TODO: This function needs to be optimized so that we do case-insensitive string comparison, and no white space string comparison.
  * @param date field value
- * @param form_id field value, referencing the selected form id.
  */
-export function addNewForm(date, form_id) {
-  const cursor = getCollection(6);
-  let uniqueDates = [];
-  cursor.forEach((doc) => uniqueDates.push(doc.name));
-  uniqueDates = _.uniq(uniqueDates);
-
-  let uniqueForm_id = [];
-  cursor.forEach((doc) => uniqueForm_id.push(doc.form_id));
-  uniqueForm_id = _.uniq(uniqueForm_id);
-
-  if (uniqueDates.includes(name) && uniqueForm_id.includes(form_id)) {
-    return false;
-  }
-    else {
-      Forms.insert({ date: date, form_id: form_id })
-      return true;
-
-    }
+export function addNewForm(date) {
+    Forms.insert({ date: date });
+    return true;
 }
 
 /**
@@ -245,84 +290,94 @@ export function addNewEvent(name, date) {
 /**
  * Always adds a new study, as there can be identical studies.
  * @param name field value
- * @param form_id field value, referencing the selected form id.
+ * @param category_id field value, referencing the selected form id.
+ * @param start_date field value, referencing the start date
+ * @param end_date field value, referencing the end date
+ *
  */
 export function addNewStudy(name, category_id, start_date, end_date) {
 
-  Categories.insert({ name: name, category_id: category_id, start_date: start_date, end_date: end_date })
+  Studies.insert({ name: name, category_id: category_id, start_date: start_date, end_date: end_date });
 
 }
 
 /**
  * Returns true if the document was added to the TrashBag collection successfully. False, otherwise.
  * This function guarantees that the Admin cannot add duplicate TrashBag. TODO: This function needs to be optimized so that we do case-insensitive string comparison, and no white space string comparison.
- * @param event field value
- * @param study_id field value, referencing the selected study id.
  * @param event_id field value, referencing the selected event id.
  * @param building_id field value, referencing the selected building id.
+ * @param location_id field value, referencing the selected location id.
  * @param category_id field value, referencing the selected category id.
  * @param form_id field value, referencing the selected form id.
- * @param accepted boolean value, whether or not it was accepted by the admin
- * @param weight field value
- * @param volume field value
- * @param count field value
+ * @param accepted boolean value, always set to false.
+ * @param weight field value.
+ * @param volume field value.
+ * @param count field value.
  */
-export function addNewTrashBag(study_id, event_id, building_id, category_id, form_id, accepted, weight, volume, count) {
+export function addNewTrashBag(event_id, building_id, location_id, category_id, form_id, accepted = false, weight, volume, count) {
   const cursor = getCollection(4);
 
-  let uniqueStudy_id = [];
-  cursor.forEach((doc) => uniqueStudy_id.push(doc.name));
-  uniqueStudy_id = _.uniq(uniqueStudy_id);
+  const date = new Date();
+  const stripped_date = date.toLocaleString('en-US');
 
+//Create a new form
+  addNewForm(stripped_date);
+  const forms_cursor = getCollection(6);
+  let formId = undefined;
+  //Find the Form ID of the Form document we just created
+  forms_cursor.forEach((doc) =>  {
+    if (doc.date === stripped_date) {
+      formId = doc._id;
+    }
+  });
   let uniqueEvent_id = [];
-  cursor.forEach((doc) => uniqueEvent_id.push(doc.name));
+  cursor.forEach((doc) => uniqueEvent_id.push(doc.event_id));
   uniqueEvent_id = _.uniq(uniqueEvent_id);
 
   let uniqueBuilding_id = [];
-  cursor.forEach((doc) => uniqueBuilding_id.push(doc.name));
+  cursor.forEach((doc) => uniqueBuilding_id.push(doc.building_id));
   uniqueBuilding_id = _.uniq(uniqueBuilding_id);
 
+  let uniqueLocation_id = [];
+  cursor.forEach((doc) => uniqueLocation_id.push(doc.location_id));
+  uniqueLocation_id = _.uniq(uniqueLocation_id);
+
   let uniqueCategory_id = [];
-  cursor.forEach((doc) => uniqueCategory_id.push(doc.name));
+  cursor.forEach((doc) => uniqueCategory_id.push(doc.category_id));
   uniqueCategory_id = _.uniq(uniqueCategory_id);
 
-  let uniqueForm_id = [];
-  cursor.forEach((doc) => uniqueForm_id.push(doc.name));
-  uniqueForm_id = _.uniq(uniqueForm_id);
-
-  let uniqueAccepted = [];
-  cursor.forEach((doc) => uniqueAccepted.push(doc.name));
-  uniqueAccepted = _.uniq(uniqueAccepted);
 
   let uniqueWeight = [];
-  cursor.forEach((doc) => uniqueWeight.push(doc.name));
+  cursor.forEach((doc) => uniqueWeight.push(doc.weight));
   uniqueWeight = _.uniq(uniqueWeight);
 
   let uniqueVolume = [];
-  cursor.forEach((doc) => uniqueVolume.push(doc.name));
+  cursor.forEach((doc) => uniqueVolume.push(doc.volume));
   uniqueVolume = _.uniq(uniqueVolume);
 
   let uniqueCount = [];
-  cursor.forEach((doc) => uniqueCount.push(doc.name));
+  cursor.forEach((doc) => uniqueCount.push(doc.count));
   uniqueCount = _.uniq(uniqueCount);
 
-    if (uniqueStudy_id.includes(study_id) && uniqueEvent_id.includes(event_id) && uniqueBuilding_id.includes(building_id) && uniqueCategory_id.includes(category_id) && uniqueForm_id.includes(form_id) && uniqueAccepted.includes(accepted) && uniqueWeight.includes(weight) && uniqueVolume.includes(volume) && uniqueCount.includes(count)) {
-      return false;}
-      else {
-                      Categories.insert({
-                        event: event,
-                        study_id: study_id,
-                        event_id: event_id,
-                        building_id: building_id,
-                        category_id: category_id,
-                        form_id: form_id,
-                        accepted: accepted,
-                        weight: weight,
-                        volume: volume,
-                        count: count
-                      })
-                      return true;
-                    }
+  if (uniqueEvent_id.includes(event_id) && uniqueLocation_id.includes(location_id) && uniqueBuilding_id.includes(building_id) && uniqueCategory_id.includes(category_id) && uniqueWeight.includes(weight) && uniqueVolume.includes(volume) && uniqueCount.includes(count)) {
+    return false;
+  }
+  else {
+    TrashBags.insert({
+      event: event,
+      event_id: event_id,
+      building_id: building_id,
+      location_id: location_id,
+      category_id: category_id,
+      form_id: formId,
+      accepted: accepted,
+      weight: weight,
+      volume: volume,
+      count: count
+    });
+    return true;
+  }
+
 }
 
 // /**
