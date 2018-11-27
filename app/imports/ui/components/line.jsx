0ae92bf7f29a4ef38adb9cb73eee5970d8c82535
Glen B <gbarcelo@users.noreply.github.com>
2018-11-26 22:37:27 -1000

    completed majority of functionality for wrapper class

--- app/imports/ui/components/line.jsx
+++ app/imports/ui/components/line.jsx
@@ -11,7 +11,20 @@ class LineGraph extends React.Component {
     };
 
     const data = {
-      labels: ['Spring 17', 'Fall 17', 'Spring 18', 'Fall 18'],
+      // labels: [
+      //     new Date(1535796000000),
+      //     new Date(1535968800000),
+      //     new Date(1536141600000),
+      //     new Date(1536314400000),
+      //     new Date(1536400800000),
+      //     new Date()
+      // ],
+      // labels: [
+      //   new Date(2018, 9, 1),
+      //   new Date(2018, 10, 1),
+      //   new Date(2018, 11, 1),
+      //   new Date(2018, 12, 1),
+      // ],
       datasets: [
         {
           label: 'By Semester',
@@ -32,15 +45,89 @@ class LineGraph extends React.Component {
           pointHoverBorderWidth: 2,
           pointRadius: 1,
           pointHitRadius: 10,
-          data: [65, 59, 80, 81, 56, 55, 40],
+          // data: [65, 59, 80, 81, 56, 55, 40],
+          data: this.props.data,
         },
       ],
     };
+    const options = {
+        scales: {
+          xAxes: [{
+            type: 'time',
+            scaleLabel: {
+              display: true,
+              labelString: 'Date'
+            }
+          }],
+          yAxes: [{
+            scaleLabel: {
+              display: true,
+              labelString: 'value'
+            }
+          }]
+        },
+    };
+
+    // const data = {
+    //     labels: [
+    //         new Date(1535796000000),
+    //         new Date(1535968800000),
+    //         new Date(1536141600000),
+    //         new Date(1536314400000),
+    //         new Date(1536400800000),
+    //         new Date()
+    //     ],
+    //     datasets: [{
+    //       label: 'My First dataset',
+    //       fill: false,
+    //       data: [65, 59, 80, 81, 56, 55, 40],
+    //     }, {
+    //       label: 'My Second dataset',
+    //       fill: false,
+    //       data: [65, 59, 80, 81, 56, 55, 40],
+    //     }, {
+    //       label: 'Dataset with point data',
+    //       fill: false,
+    //       data: [
+    //         { x: new Date(1535796000000), y: 65 },
+    //         { x: new Date(1535968800000), y: 59 },
+    //         { x: new Date(1536141600000), y: 80 },
+    //         { x: new Date(1536314400000), y: 81 },
+    //         { x: new Date(1536400800000), y: 56 },
+    //         { x: new Date(), y: 55 },
+    //       ],
+    //     }]
+    //   };
+    // const options = {
+    //     title: {
+    //       text: 'Chart.js Time Scale'
+    //     },
+    //     scales: {
+    //       xAxes: [{
+    //         type: 'time',
+    //         time: {
+    //           parser: 'MM/DD/YYYY HH:mm',
+    //           // round: 'day'
+    //           tooltipFormat: 'll HH:mm'
+    //         },
+    //         scaleLabel: {
+    //           display: true,
+    //           labelString: 'Date'
+    //         }
+    //       }],
+    //       yAxes: [{
+    //         scaleLabel: {
+    //           display: true,
+    //           labelString: 'value'
+    //         }
+    //       }]
+    //     },
+    //   };
 
     return (
         <div>
           <h3 style={styles}>Semester Data</h3>
-          <Line data={data} />
+          <Line data={data} options={options}/>
         </div>
     );
   }
