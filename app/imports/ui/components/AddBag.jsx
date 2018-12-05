d826607e86f015d454bb8a3a3e2be5b4c930059a
Hyosun Song <hyosuns@hawaii.edu>
2018-12-04 22:45:26 -1000

    addbag working, but no clear

--- app/imports/ui/components/AddBag.jsx
+++ app/imports/ui/components/AddBag.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 import { Container, Form, Grid, Header, Segment, Message } from 'semantic-ui-react';
 import { TrashBags, TrashBagSchema } from '/imports/api/TrashBags/TrashBags';
 import * as db from '../../api/Wrapper/Wrapper';
-
+import { Meteor } from 'meteor/meteor';
+import { Bert } from 'meteor/themeteorchef:bert';
 
 export default class InputForm extends React.Component {
   /** Initialize state fields. */
@@ -11,28 +12,64 @@ export default class InputForm extends React.Component {
     this.state = { event: '', building: '', location: '', category: '', form: 123, weight: '', volume: '', count: '', notes: '', accepted: false };
     this.handleSubmit = this.handleSubmit.bind(this);
     this.handleChange = this.handleChange.bind(this);
+    this.insertCallback = this.insertCallback.bind(this);
+  }
+
+  insertCallback(error) {
+    if (error) {
+      Bert.alert({ type: 'danger', message: `Add failed: ${error.message}` });
+    } else {
+      Bert.alert({ type: 'success', message: 'Add succeeded' });
+      //this.state.reset();
+    }
   }
 
   handleChange(e, { name, value }) {
     this.setState({ [name]: value });
-    console.log(this.state.form);
+    console.log(this.state);
   }
 
   handleSubmit() {
+    // this.setState({ event: '', building: '', location: '', category: '', form: 123, weight: '', volume: '', count: '', notes: '', accepted: false });
     let { event, building, location, category, form, weight, volume, count, notes } = this.state;
-    if (typeof notes === 'undefined') { notes = 'none'; }
-    db.addNewTrashBag( event, building, location, category, form, weight, volume, count, notes,
-    //     (err) => {
-    //   if (err) {
-    //     this.setState({ error: err.reason });
-    //   } else {
-    //     // browserHistory.push('/submitform');
-    //   }
-    // }
-    );
+    db.addNewTrashBag( event, building, location, category, form, weight, volume, count, notes );
+    this.insertCallback();
+
+    //this.state.reset(this.setState));
+    //this.setState.value = "";
   }
 
   render() {
+    const eventArray = db.getCollection(db.constants.codes.events);
+    const buildingArray = db.getCollection(db.constants.codes.buildings);
+    const locationArray = db.getCollection(db.constants.codes.locations);
+    const categoryArray = db.getCollection(db.constants.codes.categories);
+    const eventOptions = eventArray.map( function(event) {
+      let obj = {};
+      obj.text = event.name;
+      obj.value = event._id;
+      return obj;
+    });
+    const buildingOptions = buildingArray.map( function(building) {
+      let obj = {};
+      obj.text = building.name;
+      obj.value = building._id;
+      return obj;
+    });
+    const locationOptions = locationArray.map( function(location) {
+      let obj = {};
+      obj.text = location.name;
+      obj.value = location._id;
+      return obj;
+    });
+    const categoryOptions = categoryArray.map( function(category) {
+      let obj = {};
+      obj.text = category.name;
+      obj.value = category._id;
+      return obj;
+    });
+    console.log(eventOptions, buildingOptions, locationOptions, categoryOptions);
+
     return (
         <Container>
           <Grid textAlign="center" verticalAlign="middle" centered columns={2}>
@@ -42,28 +79,32 @@ export default class InputForm extends React.Component {
               </Header>
               <Form onSubmit={this.handleSubmit}>
                 <Segment stacked>
-                  <Form.Input
-                      label="Event"
-                      name="event"
-                      type="event"
+                  <Form.Select
+                      label='Event'
+                      name={'event'}
+                      options={eventOptions}
+                      placeholder='Event'
                       onChange={this.handleChange}
                   />
-                  <Form.Input
-                      label="Building"
-                      name="building"
-                      type="building"
+                  <Form.Select
+                      label='Building'
+                      name={'building'}
+                      options={buildingOptions}
+                      placeholder='Building'
                       onChange={this.handleChange}
                   />
-                  <Form.Input
-                      label="Location"
-                      name="location"
-                      type="location"
+                  <Form.Select
+                      label='Location'
+                      name={'location'}
+                      options={locationOptions}
+                      placeholder='Location'
                       onChange={this.handleChange}
                   />
-                  <Form.Input
-                      label="Category"
-                      name="category"
-                      type="category"
+                  <Form.Select
+                      label='Category'
+                      name={'category'}
+                      options={categoryOptions}
+                      placeholder='Category'
                       onChange={this.handleChange}
                   />
                   <Form.Input
--- app/imports/ui/pages/EditBag.jsx
+++ app/imports/ui/pages/EditBag.jsx
@@ -1,44 +1,111 @@
 import React from 'react';
-import { Grid, Header, Segment } from 'semantic-ui-react';
+import { Container, Form, Grid, Header, Segment, Message } from 'semantic-ui-react';
 import { TrashBags, TrashBagSchema } from '/imports/api/TrashBags/TrashBags';
 import * as db from '../../api/Wrapper/Wrapper';
-import AutoForm from 'uniforms-semantic/AutoForm';
-import TextField from 'uniforms-semantic/TextField';
-import NumField from 'uniforms-semantic/NumField';
-import SelectField from 'uniforms-semantic/SelectField';
-import SubmitField from 'uniforms-semantic/SubmitField';
-import HiddenField from 'uniforms-semantic/HiddenField';
-import ErrorsField from 'uniforms-semantic/ErrorsField';
 import { Meteor } from 'meteor/meteor';
-import ListBag from '../components/ListBag';
+import { Bert } from 'meteor/themeteorchef:bert';
 
 /** Renders the Page for editing a single document. */
 class EditBag extends React.Component {
   /** If the subscription(s) have been received, render the page, otherwise show a loading icon. */
   render() {
+    const eventArray = db.getCollection(db.constants.codes.events);
+    const buildingArray = db.getCollection(db.constants.codes.buildings);
+    const locationArray = db.getCollection(db.constants.codes.locations);
+    const categoryArray = db.getCollection(db.constants.codes.categories);
+    const eventOptions = eventArray.map( function(event) {
+      let obj = {};
+      obj.text = event.name;
+      obj.value = event._id;
+      return obj;
+    });
+    const buildingOptions = buildingArray.map( function(building) {
+      let obj = {};
+      obj.text = building.name;
+      obj.value = building._id;
+      return obj;
+    });
+    const locationOptions = locationArray.map( function(location) {
+      let obj = {};
+      obj.text = location.name;
+      obj.value = location._id;
+      return obj;
+    });
+    const categoryOptions = categoryArray.map( function(category) {
+      let obj = {};
+      obj.text = category.name;
+      obj.value = category._id;
+      return obj;
+    });
+    console.log(eventOptions, buildingOptions, locationOptions, categoryOptions);
+
     return (
-        <Grid container divided='vertically'>
-          <Grid.Row columns={2}>
+        <Container>
+          <Grid textAlign="center" verticalAlign="middle" centered columns={2}>
             <Grid.Column>
-              <Header as="h2" textAlign="center">Edit Bag</Header>
-              <AutoForm onSubmit={this.submit} model={this.props.doc}>
-                <Segment>
-                  <TextField name=''/>
-                  <SelectField name='type'/>
-                  <NumField name='weight' decimal={false}/>
-                  <NumField name='volume' decimal={false}/>
-                  <NumField name='count' decimal={false}/>
-                  <SubmitField value='Submit'/>
-                  <ErrorsField/>
-                  <HiddenField name='owner'/>
+              <Header as="h2" textAlign="center">
+                Manage Bags
+              </Header>
+              <Form ref={(ref) => { this.formRef = ref; }} onSubmit={this.handleSubmit}>
+                <Segment stacked>
+                  <Form.Select
+                      label='Event'
+                      name={'event'}
+                      options={eventOptions}
+                      placeholder='Event'
+                      onChange={this.handleChange}
+                  />
+                  <Form.Select
+                      label='Building'
+                      name={'building'}
+                      options={buildingOptions}
+                      placeholder='Building'
+                      onChange={this.handleChange}
+                  />
+                  <Form.Select
+                      label='Location'
+                      name={'location'}
+                      options={locationOptions}
+                      placeholder='Location'
+                      onChange={this.handleChange}
+                  />
+                  <Form.Select
+                      label='Category'
+                      name={'category'}
+                      options={categoryOptions}
+                      placeholder='Category'
+                      onChange={this.handleChange}
+                  />
+                  <Form.Input
+                      label="Weight"
+                      name="weight"
+                      type="weight"
+                      onChange={this.handleChange}
+                  />
+                  <Form.Input
+                      label="Volume"
+                      name="volume"
+                      type="volume"
+                      onChange={this.handleChange}
+                  />
+                  <Form.Input
+                      label="Count"
+                      name="count"
+                      type="count"
+                      onChange={this.handleChange}
+                  />
+                  <Form.Input
+                      label="Notes"
+                      name="notes"
+                      type="notes"
+                      onChange={this.handleChange}
+                  />
+                  <Form.Button content=" Submit"/>
                 </Segment>
-              </AutoForm>
+              </Form>
             </Grid.Column>
-            <Grid.Column>
-              <ListBag/>
-            </Grid.Column>
-          </Grid.Row>
-        </Grid>
+          </Grid>
+        </Container>
     );
   }
 }
